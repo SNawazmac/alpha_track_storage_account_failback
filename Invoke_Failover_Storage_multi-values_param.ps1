c71a302afef0346c29366d847d24ba5895ddc9fd
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-18 17:59:33 +0530

    Update Invoke_Failover_Storage_multi-values_param.ps1

--- Invoke_Failover_Storage_multi-values_param.ps1
+++ Invoke_Failover_Storage_multi-values_param.ps1
@@ -1,15 +1,15 @@
 param(
-[Parameter(Mandatory=$true)][string]$resource_group_name,       #Enter the resourcegroup name of the Storageaccount
-[Parameter(Mandatory=$true)][array]$storageaccount_names,       #Enter the storage account name(s) on which failover has to be initiated
+[Parameter(Mandatory=$true)][string]$storage_account_resource_group_name,       #Enter the resourcegroup name of the Storageaccount
+[Parameter(Mandatory=$true)][array]$storageaccount_names_private,       #Enter the storage account name(s) on which failover has to be initiated
 [Parameter(Mandatory=$true)][string]$sku                        #Enter the SKU as Standard_GRS to re-enable geo-replication post failover
 )
 
  
-$storageaccounts = $storageaccount_names.split(",")
+$storageaccounts = $storageaccount_names_private.split(",")
 foreach($storageaccount_name in $storageaccounts)
 {
 #Below command Invokes failover on the selected storage account(s)
-$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
+$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $storage_account_resource_group_name -Name $storageaccount_name -Force -AsJob
 }
 
 Wait-Job $failover
@@ -17,5 +17,5 @@ Wait-Job $failover
 foreach($storageaccount_name in $storageaccounts)
 {
 #Below command updates the SKU of the storage account(s) to Standard_GRS post failover
-Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
+Set-AzStorageAccount -ResourceGroupName $storage_account_resource_group_name -Name $storageaccount_name -SkuName $sku -Force
 }
